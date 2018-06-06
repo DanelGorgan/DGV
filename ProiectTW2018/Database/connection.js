99e9f7e40855704cb1cb813ff70049635e41c134
GorganGabi <gabigorgan@gmail.com>
2018-06-06 11:18:09 +0300

    Database connection created

---
+++ ProiectTW2018/Database/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const config = require('../config');
+
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(config.database)
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch(err => {
+        console.log(`Error while connecting to db ${err}`);
+    })
---
+++ ProiectTW2018/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/CuH',
+    port: 8050,
+    secret: 'xnbusb2/AXVZ@#%sadj827%^%13fku@$y1872822782'
+}
