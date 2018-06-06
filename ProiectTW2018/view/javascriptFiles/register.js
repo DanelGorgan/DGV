aec5f176646124c2186cccd184b2ff5563ec08b9
GorganGabi <gabigorgan@gmail.com>
2018-06-06 20:24:36 +0300

    Added user.js and finished connection with db. Some changes in register and login js files.

--- ProiectTW2018/view/javascriptFiles/register.js
+++ ProiectTW2018/view/javascriptFiles/register.js
@@ -1,3 +1,5 @@
+//var createUser = require ('../../Database/user.js')
+
 function showPassword1(){
     var password = document.getElementById('pword1');
     if (password.type == "password"){
@@ -14,4 +16,24 @@ function showPassword2(){
     } else {
         password.type="password";
     }
+}
+
+function validateForm(){
+    var un = document.getElementById('usr').value;
+    var em = document.getElementById('email').value;
+    var pw1 = document.getElementById('pword1').value;
+    var pw2 = document.getElementById('pword2').value;
+
+    console.log(pw1 + ' ' + pw2);
+    
+    if (pw1 === pw2){
+        alert("Passwords match!");
+        return true;
+    }
+    else{
+        alert("Passwords doesn't match!");
+        return false;
+    }
+
+    return true;
 }
