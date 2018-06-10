4de80d60b9a7b8f66804b167c89f19da66105080
DanelGorgan <danielgorganmihai@gmail.com>
2018-06-10 21:25:53 +0300

    ejs template engines added, refactored code

--- ProiectTW2018/app.js
+++ ProiectTW2018/app.js
@@ -1,4 +1,6 @@
-const serverHandle = require('./helpers/serverHandler.js')
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
 const config = require('./config')
 const http = require('http');
 const mongo = require('./models/connection.js')
@@ -10,7 +12,7 @@ const addRecipe = require('./controllers/addRecipe.controller')
 const search = require('./controllers/search.controller')
 const filter = require('./controllers/filter.controller')
 const latest = require('./controllers/latest.controller')
-
+const upload = require('./controllers/upload.controller')
 
 //connect with mongoose
 mongo.mongoose
@@ -23,16 +25,16 @@ http.createServer(function (req, res) {
     switch (path) {
 
         case '/recipes':
-            router.recipesRoute(req, res);
+            renderHtmlDir(req, res)
             break;
         case '/recipe':
-            router.recipesRoute(req, res);
+            renderHtmlDir(req, res)
             break;
         case '/myRecipes':
-            router.recipesRoute(req, res);
+            renderHtmlDir(req, res)
             break;
         case '/Register':
-            router.recipesRoute(req, res);
+            renderHtmlDir(req, res)
             break;
         case '/Register/submit':
             if (req.method == 'POST') {
@@ -40,23 +42,26 @@ http.createServer(function (req, res) {
             }
             break;
         case '/Login':
-            router.recipesRoute(req, res);
+            renderHtmlDir(req, res)
             break;
         case '/Login/submit':
             if (req.method == 'POST') {
                 login.login(req, res)
             }
             break;
-        case '/myAccount':
-            router.recipesRoute(req, res);
+        case '/MyAccount':
+            renderHtmlDir(req, res)
             break;
         case '/addRecipe':
-            router.recipesRoute(req, res);
+            renderHtmlDir(req, res)
             break;
         case '/addRecipe/submit':
             if (req.method == 'POST') {
                 addRecipe.addR(req, res)
             }
+            break;
+        case '/upload.ejs':
+                upload.uploading(req, res)
             break;
         case '/latest':
             if (req.method == 'POST') {
@@ -74,9 +79,82 @@ http.createServer(function (req, res) {
             }
             break;
         default:
-            serverHandle.serverHandler(req, res);
+            render(req, res);
     }
 }).listen(config.port, () => {
     console.log(`Server running at http:localhost:${config.port}/`);
 });
+
+function render(req, res) {
+    let action = req.url
+    console.log(action)
+    if(action == '/') {
+        var filePath = './view/index.ejs'
+        var htmlContent = fs.readFileSync(filePath, 'utf8');
+        var htmlRenderized = ejs.render(htmlContent);
+        res.end(htmlRenderized, 'binary')
+    }
+    let extension = req.url.substr(req.url.length - 4);
+    switch (extension) {
+        case '.png':
+            var img = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(img, 'binary');
+            break;
+        case '.jpg':
+            var img = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {'Content-Type': 'image/jpg'});
+            res.end(img, 'binary');
+            break;
+        case '.css':
+            var css = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.end(css, 'binary');
+            break;
+
+    }
+}
+
+function renderHtmlDir(req, res) {
+    let action = req.url
+
+    if(action == '/') {
+        var filePath = './view/index.ejs'
+        var htmlContent = fs.readFileSync(filePath, 'utf8');
+        var htmlRenderized = ejs.render(htmlContent);
+        res.end(htmlRenderized, 'binary')
+    } else {
+        var filePath = './view/html' + action + '.ejs'
+        console.log(filePath)
+        var htmlContent = fs.readFileSync(filePath, 'utf8');
+        var htmlRenderized = ejs.render(htmlContent);
+        res.end(htmlRenderized, 'binary')
+    }
+
+    let extension = req.url.substr(req.url.length - 4);
+    switch (extension) {
+        case '.png':
+            var img = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(img, 'binary');
+            break;
+        case '.jpg':
+            var img = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {'Content-Type': 'image/jpg'});
+            res.end(img, 'binary');
+            break;
+        case '.jpeg':
+            var img = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {'Content-Type': 'image/jpg'});
+            res.end(img, 'binary');
+            break;
+        case '.css':
+            var css = fs.readFileSync(__dirname + '/view/' + action);
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.end(css, 'binary');
+            break;
+
+    }
+}
+
 
