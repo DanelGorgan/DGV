61680c7c619901fa297eb01d3a7611c9eccd96dd
Fr33Vlad <vlady472@yahoo.com>
2018-06-11 23:20:49 +0300

    ajax sheetz

--- ProiectTW2018/controllers/latest.controller.js
+++ ProiectTW2018/controllers/latest.controller.js
@@ -4,12 +4,9 @@ module.exports.getNew = (req, res) => {
     console.log('here')
     RecipeModel.getLatest((recipes) => {
         if (recipes.length > 0) {
-            let response = []
-            for (let i = 0; i < recipes.length; i++) {
-                response.push(recipes[i].name)
-            }
+
             res.writeHead(200, {"Content-Type": "text/plain"});
-            res.end(JSON.stringify(response))
+            res.end(JSON.stringify(recipes))
         } else {
             res.writeHead(200, {"Content-Type": "text/plain"});
             res.end('Failure')
--- ProiectTW2018/view/javascriptFiles/index.js
+++ ProiectTW2018/view/javascriptFiles/index.js
@@ -1,21 +1,29 @@
 var xhr = new XMLHttpRequest();
 
-function latestRecipes(){
+function latestRecipes() {
     console.log('Am intrat in latestRecipes');
     var url = 'http://localhost:8125/latest';
-    xhr.open('GET',url);
+    xhr.open('GET', url);
     xhr.setRequestHeader("Content-type", "text/plain");
     xhr.send();
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState == XMLHttpRequest.DONE){
+    var elem = '';
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
             console.log('xhr.readyState=XMLHttpRequest.done');
             console.log(xhr.status);
-            if (xhr.status==200){
-                console.log('xhr response text is ' + xhr.responseText);
+            if (xhr.status == 200) {
+                console.log('afisam inner html si afisam ' + this.responseText)
+                var body = JSON.parse(this.responseText);
+                for (var i = 0; i < body.length; i++) {
+                    elem += "<div><figure class=\"box-img\"><img src=\"./img/img1.jpg\" alt=\"\"></figure></div><div> <p>" + body[i].description +
+                        "</p> <a href=\"http://localhost:8125/recipe\" class=\"btn\">View recipe</a> </div>";
+                }
+                document.getElementById("nr").innerHTML = elem;
             }
         }
     }
 
 
 }
+
 latestRecipes();
