9d99166db91efa1f21ac6e37d3e05697c381dda4
GorganGabi <gabigorgan@gmail.com>
2018-06-14 04:57:34 +0300

    Evaded crucial bug

--- ProiectTW2018/view/javascriptFiles/logout.js
+++ ProiectTW2018/view/javascriptFiles/logout.js
@@ -1,5 +1,6 @@
 function logout() {
     console.log('am intrat pe logout')
+    localStorage.removeItem(localStorage.key(4));
     localStorage.removeItem(localStorage.key(3));
     localStorage.removeItem(localStorage.key(2));
     localStorage.removeItem(localStorage.key(1));
