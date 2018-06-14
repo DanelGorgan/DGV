9c7dc92cad0483e0bdd8a8ebde64a2d571d325dc
GorganGabi <gabigorgan@gmail.com>
2018-06-14 12:11:34 +0300

    Slighty improvements to login and register

--- ProiectTW2018/view/javascriptFiles/register.js
+++ ProiectTW2018/view/javascriptFiles/register.js
@@ -1,60 +1,60 @@
 //var createUser = require ('../../models/user.js')
 var xhr = new XMLHttpRequest();
 
-function showPassword1(){
+function showPassword1() {
     var password = document.getElementById('pword1');
-    if (password.type == "password"){
-        password.type="text1";
+    if (password.type == "password") {
+        password.type = "text1";
     } else {
-        password.type="password";
+        password.type = "password";
     }
 }
 
-function showPassword2(){
+function showPassword2() {
     var password = document.getElementById('pword2');
-    if (password.type == "password"){
-        password.type="text1";
+    if (password.type == "password") {
+        password.type = "text1";
     } else {
-        password.type="password";
+        password.type = "password";
     }
 }
 
-function validateForm(){
+function validateForm() {
     var un = document.getElementById('usr').value;
     var em = document.getElementById('email').value;
     var pw1 = document.getElementById('pword1').value;
     var pw2 = document.getElementById('pword2').value;
 
-    if (pw1 !== pw2){
+    if (pw1 !== pw2) {
         alert("Passwords doesn't match!");
-    }
+    } else {
 
-    var url = 'http://localhost:8125/Register';
-    var data = {
-        username: un,
-        email: em,
-        password: pw1
-    };
-    console.log(data.email + ' ' + data.password + ' ' + data.username);
+        var url = 'http://localhost:8125/Register';
+        var data = {
+            username: un,
+            email: em,
+            password: pw1
+        };
+        console.log(data.email + ' ' + data.password + ' ' + data.username);
 
-    xhr.open('POST', url);
-    xhr.setRequestHeader("Content-type", "text/plain");
+        xhr.open('POST', url);
+        xhr.setRequestHeader("Content-type", "text/plain");
 
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState == XMLHttpRequest.DONE){
-            //console.log('xhr.readyState=XMLHttpRequest.done');
-            if (xhr.status==200){   
-                //console.log('xhr response text is ' + xhr.responseText);
-                if (xhr.responseText === "Success"){
-                    alert("You have succesfully registered!");
-                    window.location.href = "http://localhost:8125/Login";
-                }else{
-                    alert("User already exist!");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                //console.log('xhr.readyState=XMLHttpRequest.done');
+                if (xhr.status == 200) {
+                    //console.log('xhr response text is ' + xhr.responseText);
+                    if (xhr.responseText === "Success") {
+                        alert("You have succesfully registered!");
+                        window.location.href = "http://localhost:8125/Login";
+                    } else {
+                        alert("User already exist!");
+                    }
                 }
             }
         }
+
+        xhr.send(JSON.stringify(data));
     }
-
-    xhr.send(JSON.stringify(data));
-
 }
